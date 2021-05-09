28bb1ac05a6533fbbc9dbef8788c4f7b67bec59a
Eduardo-JReis <edu.publicidade81@gmail.com>
2021-05-09 17:52:18 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsvendas;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
